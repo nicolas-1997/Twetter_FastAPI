ad4a43da15bd95a474f88619dc4a0b93c6e468b0
nicolas-1997 <nicocabrera.0212@outlook.com>
2022-01-25 11:50:09 -0300

    path_operations_structure_users

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Python
 from dataclasses import field
 from datetime import date, datetime
-from typing import Optional
+from email.policy import HTTP
+from importlib.resources import path
+from typing import List, Optional
 from uuid import UUID
 import uuid
 from venv import create
@@ -10,7 +12,7 @@ from venv import create
 from pydantic import BaseModel, Field, EmailStr
 
 # FastAPI
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
@@ -65,6 +67,76 @@ class Tweet(BaseModel):
     update_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
+
+# Path Operations
+
 @app.get(path="/")
 def home():
     return {"Twitter API": "Working!"}
+
+## Users
+
+@app.post(
+    path="/singup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=["Users"]
+)
+def singup():
+    """
+    Sing Up
+
+    Register a User
+    """
+    pass
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a User",
+    tags=["Users"]
+)
+def login():
+    pass
+
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all Users",
+    tags=["Users"]
+)
+def show_all_users():
+    pass
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a User",
+    tags=["Users"]
+)
+def show_a_user():
+    pass
+
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="delete a User",
+    tags=["Users"]
+)
+def delete_a_user():
+    pass
+@app.put(
+    path="/users/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a Users",
+    tags=["Users"]
+)
+def update_a_user():
+    pass
+
